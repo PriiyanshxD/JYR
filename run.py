cf2b5e303e064ba7a3a4e1fad5e2d4fe0e8db9e1
PriiyanshxD <jerryking000082@gmail.com>
2025-10-05 14:42:32 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 try:
-    import menu  # ye main.cpython-312.so ko load karega
+    import Jyr  
 except ImportError as e:
     print("[ERROR] Compiled module nahi mila:", e)
     raise SystemExit
 
 # Ye function ka naam wahi hona chahiye jo tu ne .pyx/.py me entry point banaya tha
 try:
-    menu.main.menu()  # start() ko apne main code me define kiya hona chahiye
+    aprov()  # start() ko apne main code me define kiya hona chahiye
 except AttributeError:
     print("[ERROR] main.start() function nahi mila. Apna main module check karo.")
+
